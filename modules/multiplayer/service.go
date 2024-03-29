f83b768bd96811857d3bbd8e8d20f18e38c8e6fd
Chayakorn <jing182544@gmail.com>
2024-03-29 18:45:39 +0700

    [+] remove log

--- modules/multiplayer/service.go
+++ modules/multiplayer/service.go
@@ -8,14 +8,12 @@ import (
 )
 
 func ServeWs(w http.ResponseWriter, r *http.Request, isCreate bool) {
-	fmt.Println("service")
 	ws, err := hub.Upgrader.Upgrade(w, r, nil)
 	//Get room's id from client...
 	queryValues := r.URL.Query()
 	roomId := queryValues.Get("roomId")
 	roomName := queryValues.Get("roomName")
 	numberPlayer, str_err := strconv.Atoi(queryValues.Get("numberPlayer"))
-	fmt.Println("service18")
 
 	room := hub.H.Rooms[roomId]
 	if !isCreate && room.Name == "" {
@@ -29,14 +27,11 @@ func ServeWs(w http.ResponseWriter, r *http.Request, isCreate bool) {
 		if c != nil {
 			c = str_err
 		}
-		fmt.Println(err.Error())
 		ws.Close()
 	}
-	fmt.Println("service31")
 	// ws.ReadJSON(&roomId)
 
 	c := &hub.Connection{Send: make(chan []byte, 256), Ws: ws}
-	fmt.Println("service35")
 
 	s := hub.Subscription{Conn: c, RoomId: roomId, RoomName: roomName, NumberPlayer: numberPlayer}
 
