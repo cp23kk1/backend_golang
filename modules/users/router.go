70cd7427c83c1bf2f37bacdb0319ed15d845db6c
Chayakorn <jing182544@gmail.com>
2024-04-01 22:49:34 +0700

    [+] fix api update user display name

--- modules/users/router.go
+++ modules/users/router.go
@@ -19,7 +19,7 @@ func AddUserRoutes(rg *gin.RouterGroup) {
 	users.GET("/profile", GetProfile)
 	users.GET("/profile/:id", GetProfile)
 	users.GET("/statistic", GetStatistic)
-	users.PUT("/statistic", UpdateProfile)
+	users.PUT("/profile", UpdateProfile)
 }
 
 func GetProfile(c *gin.Context) {
